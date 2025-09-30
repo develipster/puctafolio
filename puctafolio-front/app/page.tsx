f2fbecf9c9e9423396a451ecf86d64517e65d5a9
PipeOlivares <felipe.olivares@uc.cl>
2025-09-30 19:16:26 -0300

    Initial Commit: Portfolio Template

---
+++ puctafolio-front/app/page.tsx
@@ -0,0 +1,22 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        PUC-tafolio by <i>develipster</i>
+      </h1>
+      <p className="mb-4">
+        {`I'm a recent Computer Engineering graduate with expertise in full-stack development,
+        cloud computing, and data-driven solutions. Proven track record in developing scalable
+        web applications using modern technologies including React.js, Node.js, Python, and AWS.
+        Strong foundation in software architecture, process mining, and artificial intelligence.
+        Passionate about creating efficient, well-documented solutions that drive business value
+        and optimize user experiences.`}
+      </p>
+      <div className="my-8">
+        <BlogPosts />
+      </div>
+    </section>
+  )
+}
